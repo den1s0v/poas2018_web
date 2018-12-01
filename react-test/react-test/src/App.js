37ca8b37a29015e9c02e6bdca171470829dfe2af
den1s0v <hello-lumen@ya.ru>
2018-12-01 13:31:45 +0300

    react test app & functional component on top of page

---
+++ react-test/react-test/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// функциональный компонент
+function ComponentName(props) {
+  return (
+    <div>
+		Hello, {props.name}
+    </div>
+  )
+
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <ComponentName name="Vasya" />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
