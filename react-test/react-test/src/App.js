2681cdbbc44ac7a65a9b3016b6a8a1ab050e4d11
den1s0v <hello-lumen@ya.ru>
2018-12-01 14:03:06 +0300

    ways to provide parameters

--- react-test/react-test/src/App.js
+++ react-test/react-test/src/App.js
@@ -7,7 +7,11 @@ function ComponentName(props) {
 console.log(props);
   return (
     <div>
-		Hello, {props.name} {props.surname}
+		Hello, {props.name}
+		<br />
+		  {props.info.surname}
+		<br />
+		{props.callback('Message')}
     </div>
   )
 
@@ -18,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <ComponentName name="Vasya" surname="Pupkin" />
+      <ComponentName name="Vasya" info={{surname:"Pupkin", adress:"Volg"}} callback={text => <small>{text}</small>} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
