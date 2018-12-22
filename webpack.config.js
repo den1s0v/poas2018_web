cb16fd5881bc14c9285a97d55835da2d79f414b4
den1s0v <hello-lumen@ya.ru>
2018-12-22 11:06:22 +0400

    ещё пакеты

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
+function getStyleLoaders(cssOptions = {}, preProcessor) {
+  const loaders = [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: cssOptions
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+        ]
+      }
+    }
+  ]
+  if (preProcessor) loaders.push(preProcessor);
+  return loaders;
+}
+
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
