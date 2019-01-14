41a9785c6e5b507deae7147b92ad0749ebcf4f80
den1s0v <hello-lumen@ya.ru>
2019-01-14 19:26:56 +0400

    Идеи по структуре контента

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 // import { Router, Route } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
 import React, { Component } from "react";
+
 import styles from "./index.scss";
 import App from "./components/App";
 
@@ -11,3 +12,31 @@ ReactDOM.render(
       </Router>),
   document.getElementById('root')
 )
+
+console.log('index begin');
+
+
+
+/***********
+
+{User}
+ - name: str
+ - pass: str?
+ - stars: int
+ - ownedSamples[]
+ - solvedSamples[]
+
+{ Sample (Task) }
+ - ownerId: str
+ - regex[userId -> str]  --- правильный  паттерн, придуманный пользователем (решил, если присутствует в этом списке)
+  - отображение:
+    - editablePattern:str
+    - isPatternValid: boolean
+ - stars: int  --- сложность задания
+ - regexLenLimit: int  --- ограничение на длину регулярки (задание)
+ - checkLines[] --- проверочные примеры
+   - хранение: { str: str, mustMatch: true/false/undefined }  --- null для mustMatch означает по умолчанию от паттерна
+   - отображение (решение)  +: { isOkMatch:true/false/undefined }
+   - отображение (редактор) +: { edit:true, onRemoveCallback:func }
+
+************/
