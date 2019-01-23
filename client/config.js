730fc1b589c5317979716b9a6bd1c1690734f116
den1s0v <hello-lumen@ya.ru>
2019-01-23 15:44:53 +0400

    Мой идентификатор Гугл-приложения

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 const config = {
   google: {
-    clientID: '799189369589-qk7dt2636iadrt9k9ghjvn44kadfut2n.apps.googleusercontent.com'
+    clientID: '289814883390-vqr4kc66i7e4n5urmahi4orl79ppdrhs.apps.googleusercontent.com'
   },
   vk: {
     vkUrl: 'https://oauth.vk.com/authorize?client_id=6789814&display=popup&redirect_uri=http://localhost:3000/api/auth/vk&scope=friends,email&response_type=code&v=5.92'
