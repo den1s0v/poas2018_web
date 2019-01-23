9b032b0f01cdaa7143d9beecb838614ff9a2b588
den1s0v <hello-lumen@ya.ru>
2019-01-24 01:24:26 +0400

    Подгрузка семплов перенесена из App в Index

--- client/components/App.js
+++ client/components/App.js
@@ -13,7 +13,6 @@ import SamplePanel  from "./SamplePanel";
 console.log('app begin');
 
 import SampleData from '../../models/SampleData';
-import makeCancelable from '../../services/make-promise-cancelable';
 
 
 
@@ -24,9 +23,9 @@ class App extends Component {
     this.onLogIn = this.onLogIn.bind(this);
     this.logout = this.logout.bind(this);
     
-    this.state = {
-      samples: null
-    };
+    // this.state = {
+      // samples: null
+    // };
   }
   
 	onLogIn() {
@@ -45,8 +44,8 @@ class App extends Component {
 	}
 
   render() {
-    const samples = this.state.samples;
-    console.log('component App render():', samples && samples.length || 0,'samples.');
+    // const samples = this.state.samples;
+    // console.log('component App render():', samples && samples.length || 0,'samples.');
     
     return (
       <>
@@ -61,11 +60,11 @@ class App extends Component {
         
         <Route path="/" exact component={Index} />
 
+        {/* 
         <Route path="/quiz" exact component={ () => 
           <SamplePanel sample={ samples && samples[0] } isEdit={true} />
         } />
         
-        {/* 
           show={true}
         <Index />
         < Route path="/user" component={ () => <Users show={true}/>} />
@@ -75,28 +74,28 @@ class App extends Component {
     )
   }
   
-  componentDidMount() {
-    console.log('component App DidMount');
+  // componentDidMount() {
+    // console.log('component App DidMount');
     
-    this.cancelableRecieve = makeCancelable( SampleData.fetchSamples() );
-    this.cancelableRecieve.promise
-      .then(samples => {
-        console.log('component App setState():', samples.length,'samples.');
+    // this.cancelableRecieve = makeCancelable( SampleData.fetchSamples() );
+    // this.cancelableRecieve.promise
+      // .then(samples => {
+        // console.log('component App setState():', samples.length,'samples.');
         
-        this.setState({
-          samples: samples
-        })
-      })
-      .catch((reason) => console.log('isCanceled', reason.isCanceled));
-  }
+        // this.setState({
+          // samples: samples
+        // })
+      // })
+      // .catch((reason) => console.log('isCanceled', reason.isCanceled));
+  // }
   
-  componentWillUnmount() {
+  // componentWillUnmount() {
     
-    console.log('component App WillUnmount');
-    if(this.cancelableRecieve) {
-      this.cancelableRecieve.cancel();
-    }
-  }
+    // console.log('component App WillUnmount');
+    // if(this.cancelableRecieve) {
+      // this.cancelableRecieve.cancel();
+    // }
+  // }
 }
 export default hot(App);
 
--- client/components/Index.js
+++ client/components/Index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import { Badge } from "react-bootstrap";
+// import { Badge } from "react-bootstrap";
 
 import { SampleList } from "./SampleList";
+import SamplePanel  from "./SamplePanel";
+
+import makeCancelable from '../../services/make-promise-cancelable';
+import SampleData from '../../models/SampleData';
+
 
 export class Index extends Component {
   constructor(props, context) {
@@ -11,17 +16,49 @@ export class Index extends Component {
     // this.logout = this.logout.bind(this);
     
     this.state = {
-      samples: null
+      // samples arrays
+      quiz: null,
+        my: null,
+      done: null,
     };
   }
 
   render() {
+    console.log('component Index render()',this.state)
     return (
     <>
-      <SampleList title={"Нерешенные задачи"} samples={[]} />
-      <SampleList title={"Мои задачи"} samples={[]} />
-      <SampleList title={"Решенные задачи"} samples={[]} />
+      <SampleList title={"Нерешенные задачи"} samples={this.state.quiz} />
+      <SampleList title={"Мои задачи"}        samples={this.state.my  } />
+      <SampleList title={"Решенные задачи"}   samples={this.state.done} />
     </>
     );
+  }
+  
+  componentDidMount() {
+    // console.log('component Index DidMount');
+    
+    this.cancelableRecieve = {};
+    
+    for (const _key in this.state) {
+      const key = _key;
+      this.cancelableRecieve[key] = makeCancelable( SampleData.fetchSamples(key) );
+      this.cancelableRecieve[key].promise
+        .then(samples => {
+          this.setState({
+            [key]:samples
+          })
+          console.log('component Index, Mode "'+key+'":', samples.length,'samples.');
+        })
+        .catch((reason) => console.log('isCanceled', reason.isCanceled));
+    }
+  }
+  
+  componentWillUnmount() {
+    console.log('component Index WillUnmount');
+    if(this.cancelableRecieve) {
+      for (let key in this.cancelableRecieve) {
+         this.cancelableRecieve[key].cancel();
+      }
+    }
   }
 }
